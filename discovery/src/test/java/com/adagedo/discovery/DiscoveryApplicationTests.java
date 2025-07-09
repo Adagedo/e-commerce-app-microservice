730c484dc9178b5a4a5fec1f2c0127f6d0a02fde
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-07-09 16:26:24 +0100

    created the discovery service

---
+++ discovery/src/test/java/com/adagedo/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adagedo.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
