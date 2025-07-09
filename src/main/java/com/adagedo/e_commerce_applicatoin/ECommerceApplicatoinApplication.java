8c3965e21cce63370ac3d4ac9f16e46b8d0af709
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-07-09 15:39:43 +0100

    initial commit

---
+++ src/main/java/com/adagedo/e_commerce_applicatoin/ECommerceApplicatoinApplication.java
@@ -0,0 +1,13 @@
+package com.adagedo.e_commerce_applicatoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplicatoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplicatoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adagedo/e_commerce_applicatoin/ECommerceApplicatoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adagedo.e_commerce_applicatoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicatoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
