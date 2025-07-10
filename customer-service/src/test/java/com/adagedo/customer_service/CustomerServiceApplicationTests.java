b03f2f9bc5d424273be260aea00aaea429b5f029
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-07-10 16:42:33 +0100

    added the customer service

---
+++ customer-service/src/test/java/com/adagedo/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adagedo.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
